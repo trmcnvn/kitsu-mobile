618515842812cdd2c393f40d152b8dc949d12f55
Thomas McNiven <hello@vevix.net>
2019-01-14 11:02:43 +1000

    Update Android to latest JSC

--- src/screens/Profiles/components/UserStats/component.js
+++ src/screens/Profiles/components/UserStats/component.js
@@ -30,12 +30,15 @@ export class UserStats extends PureComponent {
   };
 
   displayGenres(data) {
-    const array = Object.keys(data.categories).map(key => ({ name: key, percent: data.categories[key] / data.total * 100 }));
+    const array = Object.keys(data.categories).map(key => ({
+      name: key,
+      percent: (data.categories[key] / data.total) * 100,
+    }));
     const sorted = array.sort(({ percent: a }, { percent: b }) => b - a).slice(0, 7);
     const filtered = sorted.filter(({ percent }) => Math.round(percent) > 0);
     return filtered.map((datum, idx) => ({
       ...datum,
-      relativeSize: (datum.percent / filtered[0].percent * 100),
+      relativeSize: (datum.percent / filtered[0].percent) * 100,
       color: COLORS[idx % COLORS.length],
     }));
   }
@@ -43,8 +46,12 @@ export class UserStats extends PureComponent {
   categoryBreakdown(data) {
     const displayGenres = this.displayGenres(data);
     const radius = PIE_SIZE / 2;
-    const outerArc = arc().outerRadius(radius).innerRadius(radius - 20);
-    const chart = pie().sort(null).value(({ percent }) => percent);
+    const outerArc = arc()
+      .outerRadius(radius)
+      .innerRadius(radius - 20);
+    const chart = pie()
+      .sort(null)
+      .value(({ percent }) => percent);
     const primaryGenre = displayGenres[0];
     return (
       <View style={styles.graphics}>
@@ -57,24 +64,34 @@ export class UserStats extends PureComponent {
                 <Group x={radius} y={radius}>
                   <Shape d={path} fill={arc.data.color} />
                 </Group>
-              )
+              );
             })}
           </Surface>
           <View style={styles.categoryOverlay}>
-            <StyledText bold size="xlarge" color="dark">{`${Number(primaryGenre.percent || 0).toFixed(0)}%`}</StyledText>
-            <StyledText bold size="xxsmall" color="grey" numberOfLines={1}>{primaryGenre.name}</StyledText>
+            <StyledText bold size="xlarge" color="dark">{`${Number(primaryGenre.percent || 0).toFixed(
+              0,
+            )}%`}</StyledText>
+            <StyledText bold size="xxsmall" color="grey" numberOfLines={1}>
+              {primaryGenre.name}
+            </StyledText>
           </View>
         </View>
         {/* Legend */}
         <View style={styles.legendWrap}>
           {displayGenres.map(genre => (
             <View style={{ flexDirection: 'row' }}>
-              <StyledText size="xsmall" textStyle={{ color: genre.color }}>{`${Number(genre.percent || 0).toFixed(0)}%`}</StyledText>
+              <StyledText size="xsmall" textStyle={{ color: genre.color }}>{`${Number(genre.percent || 0).toFixed(
+                0,
+              )}%`}</StyledText>
               <View style={{ flex: 1, alignSelf: 'stretch', paddingLeft: 10, paddingRight: 5 }}>
-                <StyledText bold size="xsmall" color="grey" numberOfLines={1}>{genre.name}</StyledText>
+                <StyledText bold size="xsmall" color="grey" numberOfLines={1}>
+                  {genre.name}
+                </StyledText>
               </View>
               <View style={{ flex: 1, justifyContent: 'center' }}>
-                <View style={{ width: `${genre.relativeSize}%`, height: 6, backgroundColor: genre.color, borderRadius: 8 }} />
+                <View
+                  style={{ width: `${genre.relativeSize}%`, height: 6, backgroundColor: genre.color, borderRadius: 8 }}
+                />
               </View>
             </View>
           ))}
@@ -95,9 +112,15 @@ export class UserStats extends PureComponent {
       }
     }
     const chapters = data.units;
-    if (chapters > 15000) { return { index: 4, count: chapters }; }
-    if (chapters > 5000) { return { index: 3, count: chapters }; }
-    if (chapters > 1000) { return { index: 2, count: chapters }; }
+    if (chapters > 15000) {
+      return { index: 4, count: chapters };
+    }
+    if (chapters > 5000) {
+      return { index: 3, count: chapters };
+    }
+    if (chapters > 1000) {
+      return { index: 2, count: chapters };
+    }
     return { index: 1, count: chapters };
   }
 
@@ -113,7 +136,12 @@ export class UserStats extends PureComponent {
   timeSpent(kind, data) {
     const primaryUnit = this.primaryUnit(kind, data);
     const percentile = this.percentile(kind, data);
-    const timeUnitText = kind === 'anime' ? primaryUnit.count === 1 ? primaryUnit.name.slice(0, primaryUnit.name.length - 1) : primaryUnit.name : null;
+    const timeUnitText =
+      kind === 'anime'
+        ? primaryUnit.count === 1
+          ? primaryUnit.name.slice(0, primaryUnit.name.length - 1)
+          : primaryUnit.name
+        : null;
     return (
       <View>
         <FastImage
@@ -125,38 +153,44 @@ export class UserStats extends PureComponent {
         <View style={styles.timeSpent}>
           <View style={{ flexDirection: 'column', marginLeft: 60 }}>
             <StyledText bold size="xsmall" color="dark" textStyle={{ marginBottom: 5 }}>
-              {/* toLocaleString doesn't work on Android */}
-              {primaryUnit.count.toString().includes('.') ? primaryUnit.count.toFixed(1) : primaryUnit.count.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
-              {' '}
-              {kind === "anime" ? `${timeUnitText} spent watching anime.` : "chapters read."}
+              {primaryUnit.count.toLocaleString('en-US', { maximumFractionDigits: 1 })}{' '}
+              {kind === 'anime' ? `${timeUnitText} spent watching anime.` : 'chapters read.'}
             </StyledText>
             <StyledText size="xxsmall" color="grey">
-              {data.completed > 0 && (`${data.completed.toLocaleString('en-US')} completed. `)}
+              {data.completed > 0 && `${data.completed.toLocaleString('en-US')} completed. `}
               {percentile > 0 && (
                 <React.Fragment>
-                  {"More than "}
+                  {'More than '}
                   <StyledText bold size="xxsmall" color="darkGrey">{`${percentile.toFixed(0)}% `}</StyledText>
-                  {"of users."}
+                  {'of users.'}
                 </React.Fragment>
               )}
             </StyledText>
           </View>
         </View>
       </View>
-    )
+    );
   }
 
   // header: "Looks like you don't have enough data yet! Ready to start something new?"
   //       guest-header: "Looks like {user} doesn't have any stats yet."
   render() {
     const { kind, data, loading, isCurrentUser } = this.props;
-    if (Object.keys(data).length === 0) { return null; }
+    if (Object.keys(data).length === 0) {
+      return null;
+    }
 
     const amountConsumed = data[`${kind}-amount-consumed`];
     const categoryBreakdown = data[`${kind}-category-breakdown`];
-    if (!amountConsumed || Object.keys(amountConsumed).length === 0) { return null; }
-    if (kind === 'anime' && amountConsumed.statsData.time < 1) { return null; }
-    if (kind === 'manga' && amountConsumed.statsData.units < 1) { return null; }
+    if (!amountConsumed || Object.keys(amountConsumed).length === 0) {
+      return null;
+    }
+    if (kind === 'anime' && amountConsumed.statsData.time < 1) {
+      return null;
+    }
+    if (kind === 'manga' && amountConsumed.statsData.units < 1) {
+      return null;
+    }
 
     return (
       <View style={styles.wrap}>
@@ -170,16 +204,20 @@ export class UserStats extends PureComponent {
             </View>
           </View>
         ) : (
-            <React.Fragment>
-              {categoryBreakdown && categoryBreakdown.statsData.total >= 1 ? this.categoryBreakdown(categoryBreakdown.statsData) : (
-                <StyledText size="xsmall" color="dark" textStyle={{ textAlign: 'center' }}>
-                  {isCurrentUser ? "Looks like you don't have enough data yet! Ready to start something new?" : "Looks like this user doesn't have any stats at the moment."}
-                </StyledText>
-              )}
-              {this.timeSpent(kind, amountConsumed.statsData)}
-            </React.Fragment>
-          )}
+          <React.Fragment>
+            {categoryBreakdown && categoryBreakdown.statsData.total >= 1 ? (
+              this.categoryBreakdown(categoryBreakdown.statsData)
+            ) : (
+              <StyledText size="xsmall" color="dark" textStyle={{ textAlign: 'center' }}>
+                {isCurrentUser
+                  ? "Looks like you don't have enough data yet! Ready to start something new?"
+                  : "Looks like this user doesn't have any stats at the moment."}
+              </StyledText>
+            )}
+            {this.timeSpent(kind, amountConsumed.statsData)}
+          </React.Fragment>
+        )}
       </View>
     );
   }
-};
+}
